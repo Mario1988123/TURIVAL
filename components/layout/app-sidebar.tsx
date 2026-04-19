a01e580f49938f1f04d240f33ecff3c515363ba5
Mario1988123 <mario.ortigueira@me.com>
2026-04-19 20:05:15 +0200

    Add files via upload

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -12,6 +12,7 @@ import {
   ShoppingCart,
   Package,
   Palette,
+  Layers,
   QrCode,
   Users,
   Factory,
@@ -33,6 +34,7 @@ const MENU_ITEMS = [
   { label: 'Albaranes', href: '/albaranes', icon: Truck },
   { label: 'Productos', href: '/productos', icon: Package },
   { label: 'Colores', href: '/colores', icon: Palette },
+  { label: 'Tratamientos', href: '/tratamientos', icon: Layers },
   { label: 'Trazabilidad', href: '/trazabilidad', icon: QrCode },
   { label: 'Informes', href: '/informes', icon: BarChart3 },
 ]
