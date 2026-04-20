0d3b45290a7b8b94f6c480c24623c34e92e6cc3d
Mario1988123 <mario.ortigueira@me.com>
2026-04-20 13:43:08 +0200

    Add files via upload

---
+++ app/presupuestos/layout.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { AppLayout } from '@/components/layout/app-sidebar'
+
+export default function PresupuestosLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <AppLayout title="Presupuestos">
+      {children}
+    </AppLayout>
+  )
+}
