cdd4bb20ddcce5a4221bb8523ee86b6d1d136038
Mario1988123 <mario.ortigueira@me.com>
2026-04-22 00:27:06 +0200

    Delete app/materiales/materiales-layout.tsx

--- app/materiales/materiales-layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppLayout } from '@/components/layout/app-sidebar'
-
-export default function MaterialesLayout({ children }: { children: React.ReactNode }) {
-  return <AppLayout title="Materiales">{children}</AppLayout>
-}
