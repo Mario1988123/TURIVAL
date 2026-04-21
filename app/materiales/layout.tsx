46294538acc6279158936b14f5ed305131404b3f
Mario1988123 <mario.ortigueira@me.com>
2026-04-22 00:26:50 +0200

    Update layout.tsx

--- app/materiales/layout.tsx
+++ app/materiales/layout.tsx
@@ -1,1 +1,5 @@
+import { AppLayout } from '@/components/layout/app-sidebar'
 
+export default function MaterialesLayout({ children }: { children: React.ReactNode }) {
+  return <AppLayout title="Materiales">{children}</AppLayout>
+}
